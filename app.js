910a9a1277886d3231779184209c171c1c5e1a88
Joshua Paul <mars@joshuas-mbp.mynetworksettings.com>
2023-03-16 17:48:35 -0400

    I have finished the attack sequences and the basic game is complete

--- app.js
+++ app.js
@@ -1,54 +1,71 @@
 //Create a ship with hull, firepower, accuarcy, and team
 
-class Ship{
-  constructor(hull, firepower, team){
+class Ship {
+  constructor(team, name, hull = null, firepower = null) {
     this.team = team
-    if (this.team === 'good'){
+    this.name = name
+    if (this.team === 'good') {
       this.hull = hull
       this.firePower = firepower
       this.accuracy = () => {
-        if (Math.random() < .7){
-          console.log('you hit it')
+        if (Math.random() < .7) {
           return true
         } else {
-          console.log('no')
+          console.log(this.name + " missed")
           return false
         }
       }
     } else if (this.team === 'bad') {
-      this.hulls = [3,4,5,6]
-      this.firePowers = [2,3,4]
-      this.accuracies = [.6,.7,.8]
-      this.hull = this.hulls[Math.floor(Math.random() * 3)]
-      this.firePower = this.firePowers[Math.floor(Math.random() * 2)]
+      this.hulls = [3, 4, 5, 6]
+      this.firePowers = [2, 3, 4]
+      this.accuracies = [.6, .7, .8]
+      this.hull = this.hulls[Math.floor(Math.random() * 4)]
+      this.firePower = this.firePowers[Math.floor(Math.random() * 3)]
       this.int = this.accuracies[Math.floor(Math.random() * 3)]
       this.accuracy = () => {
-       if (Math.random() < this.int){
-        console.log('you hit it')
-        return true
-       } else {
-        return false
-       }
+        if (Math.random() < this.int) {
+          return true
+        } else {
+          console.log(this.name + " missed")
+          return false
+        }
       }
     }
-
+    this.shipDestroyed = 0
   }
-
+  //Created attack function Fundamentals
   attack(target){
+   //Attack a ship
    if (this.accuracy()){
-    console.log(this.firePower)
     target.hull -= this.firePower
-
+    console.log(`${this.name} hit ${target.name} and the ship had ${target.hull + this.firePower} and now have ${target.hull} health left` )
+    if (target.hull <= 0){
+      this.shipDestroyed++
+      console.log(this.shipDestroyed)
+      if (this.shipDestroyed === 6){
+        return "I am done"
+      }
+      if (this.team === 'good'){
+      console.log('choose to retreat')
+      let p = window.prompt('Would you like to retreat')
+      if (p === 'no'){
+       target = new Ship('bad', 'enemyship')
+       this.attack(target)
+      } else if(p === 'yes') {
+        console.log('You have retreated')
+      }        
+      } else  if (this.team === 'bad'){
+        console.log('You have been destroyed')
+      }
+    } else {
+      target.attack(this)
+    }
    } else {
-
+    target.attack(this)
    }
   }
-
 }
 
-
-let enemyShip1 = new Ship(null, null, 'bad')
-console.log(enemyShip1)
-let uSS = new Ship(20,5,'good')
-console.log(uSS)
-uSS.attack(enemyShip1)
+uSS = new Ship('good','uss', 20, 5)
+alienShip1 = new Ship('bad', 'enemy')
+uSS.attack(alienShip1)
